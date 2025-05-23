f9d287d01c0170631107a1273b6c7a3574ed5159
Sandoval <gtkaiser29@gmail.com>
2025-05-23 12:27:42 -0600

    Código CentroGrafo
    Creación de código para calcular el centro del grafo

--- CentroGrafo.java
+++ CentroGrafo.java
@@ -1,3 +1,27 @@
 public class CentroGrafo {
-    
-}
+
+     
+      public static int calcularCentro(double[][] distancias) {
+          int n = distancias.length;
+          double minExcentricidad = Double.POSITIVE_INFINITY;
+          int centro = -1;
+  
+          for (int i = 0; i < n; i++) {
+              double excentricidad = 0;
+  
+              for (int j = 0; j < n; j++) {
+                  if (i != j && distancias[i][j] != Double.POSITIVE_INFINITY) {
+                      excentricidad = Math.max(excentricidad, distancias[i][j]);
+                  }
+              }
+  
+              if (excentricidad < minExcentricidad) {
+                  minExcentricidad = excentricidad;
+                  centro = i;
+              }
+          }
+  
+          return centro;
+      }
+  }
+  
