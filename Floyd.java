45375e868a5f8d1e280e617c4a869d75b81856e0
Sandoval <gtkaiser29@gmail.com>
2025-05-23 12:22:48 -0600

    Código de Floyd
    Se completo el código de floyd.java agregando las implementaciones finales.

--- Floyd.java
+++ Floyd.java
@@ -1,3 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Floyd {
-    
+    private double[][] distancias;
+    private int[][] caminos;
+    private int tamaño;
+
+    public Floyd(double[][] matrizPesos) {
+        tamaño = matrizPesos.length;
+        distancias = new double[tamaño][tamaño];
+        caminos = new int[tamaño][tamaño];
+        floydWarshall(matrizPesos);
+    }
+
+    private void floydWarshall(double[][] grafo) {
+        for (int i = 0; i < tamaño; i++) {
+            for (int j = 0; j < tamaño; j++) {
+                distancias[i][j] = grafo[i][j];
+                caminos[i][j] = (grafo[i][j] != Double.POSITIVE_INFINITY && i != j) ? j : -1;
+            }
+        }
+
+        for (int k = 0; k < tamaño; k++) {
+            for (int i = 0; i < tamaño; i++) {
+                for (int j = 0; j < tamaño; j++) {
+                    if (distancias[i][k] + distancias[k][j] < distancias[i][j]) {
+                        distancias[i][j] = distancias[i][k] + distancias[k][j];
+                        caminos[i][j] = caminos[i][k];
+                    }
+                }
+            }
+        }
+    }
+
+    public double[][] getDistancias() {
+        return distancias;
+    }
+
+    public int[][] getCaminos() {
+        return caminos;
+    }
+
+    public List<Integer> reconstruirCamino(int origen, int destino) {
+        List<Integer> camino = new ArrayList<>();
+        if (caminos[origen][destino] == -1) return camino;
+        camino.add(origen);
+        while (origen != destino) {
+            origen = caminos[origen][destino];
+            camino.add(origen);
+        }
+        return camino;
+    }
 }
